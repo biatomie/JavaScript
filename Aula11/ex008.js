6eda0b133835e4e815c83f930aefcf384c714954
biatomie <bia.tomie@outlook.com>
2021-08-01 09:22:41 -0300

    primeiro envio

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78
+console.log(`A velocidade do seu carro pe ${vel}Km/h`)
+if (vel > 60){ //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+else{
+    console.log(`Dirija sempre usando cinto de segurança!`)
+}
